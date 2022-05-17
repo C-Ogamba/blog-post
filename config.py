c54946f1273423cbafc53a4d601442e893bcba82
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-05-17 07:39:25 +0300

    Dev: update the entry file

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    """
+    Common configurations
+    """
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+        'postgres://', 'postgresql://') or\
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLACHEMY_TRACK_MODIFICATIONS = False
+
+    #Pagination
+    POSTS_PER_PAGE = int(os.environ.get('POSTS_PER_PAGE') or 10)
+
+    #Heroku logS
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+    
