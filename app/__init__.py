05a272ed7b16bed0e1d5a0e4fba523c7837376b0
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-05-20 20:10:55 +0300

    Dev: Add username and email validation

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_moment import Moment
+from flask_bcrypt import Bcrypt
 import logging
 from logging.handlers import RotatingFileHandler
 import os
@@ -14,5 +15,6 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 moment = Moment(app)
+bcrypt = Bcrypt(app)
 
 from app import routes, models
