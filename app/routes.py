4528a2b8d5492677964a68e049a7ed0e3d67017c
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-05-20 15:52:22 +0300

    Dev: style our forms

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from crypt import methods
 from app import app
 from flask import Flask, render_template, url_for
+from app.forms import LoginForm, RegistrationForm
 
 
 
@@ -8,3 +9,13 @@ from flask import Flask, render_template, url_for
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
