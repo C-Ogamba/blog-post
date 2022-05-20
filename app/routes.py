5e6ab66b9dba5c7c6773c3a18375e685dc209271
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-05-20 17:20:07 +0300

    Dev: create and add routing to the different pages

--- app/routes.py
+++ app/routes.py
@@ -11,6 +11,10 @@ from app.forms import LoginForm, RegistrationForm
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
@@ -19,7 +23,13 @@ def register():
         return redirect(url_for('index'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Login Unsuccessfull. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
