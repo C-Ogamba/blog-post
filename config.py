61be33db70d6d554db8e4431cc0ca3fd962b2fa7
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-05-20 18:02:57 +0300

    Dev: add and create db

--- config.py
+++ config.py
@@ -7,4 +7,7 @@ class Config(object):
     Common configurations
     """
     SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+        'postgres://', 'postgresql://') or\
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLACHEMY_TRACK_MODIFICATIONS = False
