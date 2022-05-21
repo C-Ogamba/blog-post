af188ebec7414d90050a52e6d78fc2cf4bbf148f
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-05-21 06:27:35 +0300

    Feat: styling the post blog

--- app/routes.py
+++ app/routes.py
@@ -102,3 +102,8 @@ def new_post():
         return redirect(url_for('index'))
     return render_template('create_post.html', title='New post', form=form)
 
+@app.route('/post/<post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
